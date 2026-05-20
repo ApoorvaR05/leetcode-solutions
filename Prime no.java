2e73a4d30b69652f87e30086f2781d050b9b9656
Apoorva R <135038725+ApoorvaR05@users.noreply.github.com>
2026-05-20 19:26:40 +0530

    Create Prime no.java

---
+++ Prime no.java
@@ -0,0 +1,18 @@
+Time: O(sqrt n)
+Space: O(1)
+
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        if (n <= 1) {
+            return false;
+        }
+        
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
