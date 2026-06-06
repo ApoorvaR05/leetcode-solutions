e7c08a7d6a88b76172e976ced3f334c9af687640
Apoorva R <135038725+ApoorvaR05@users.noreply.github.com>
2026-06-06 09:52:43 +0530

    Create Inorder traversal(BT).java

---
+++ Inorder traversal(BT).java
@@ -0,0 +1,42 @@
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(res, root);
+        return res;
+
+    }
+    private void inorder(List<Integer> res, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(res, root.left);
+        res.add(root.val);
+        inorder(res, root.right);
+    }
+}
+
+/*
+| Traversal         | Time | Space    |
+| ----------------- | ---- | -------- |
+| Recursive Inorder | O(n) | O(h)     |
+| Iterative Inorder | O(n) | O(h)     |
+| Balanced Tree     | O(n) | O(log n) |
+| Skewed Tree       | O(n) | O(n)     |
+
+  "Each node is visited once, giving O(n) time, and the recursion/stack stores at most the tree height h, giving O(h) space." */
