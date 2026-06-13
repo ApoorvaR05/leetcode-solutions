04d246030073e6abffeeea2397d224ec02cee563
Apoorva R <135038725+ApoorvaR05@users.noreply.github.com>
2026-06-13 09:42:52 +0530

    Create Subarray sum equals k.java

---
+++ Subarray sum equals k.java
@@ -0,0 +1,29 @@
+/* Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+A subarray is a contiguous non-empty sequence of elements within an array.
+Example 1:
+Input: nums = [1,1,1], k = 2
+Output: 2
+Time: O(n)
+Space: O(n) */
+
+import java.util.*;
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int count = 0;
+        map.put(0,1);
+
+        for (int num:nums) {
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
+/* check whether subarray sum = prefixSum - k */
