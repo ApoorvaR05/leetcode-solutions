d8a101d981d29ecdc3e1f360ab191ebf711d0342
Apoorva R <135038725+ApoorvaR05@users.noreply.github.com>
2026-07-04 15:52:10 +0530

    Create Missing number.java

---
+++ Missing number.java
@@ -0,0 +1,29 @@
+/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+time: O(n), space: O(1)*/
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int s = (n * (n + 1)) / 2;
+        int sum = 0;
+
+        for (int num: nums) {
+            sum += num;
+        }
+        return s - sum;
+    }
+}
+/////
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= i;
+            xor ^= nums[i];
+        }
+
+        return xor;
+    }
+}
