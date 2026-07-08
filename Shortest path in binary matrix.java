588984da929725d7f430d77f83cdb38faaebe833
Apoorva R <135038725+ApoorvaR05@users.noreply.github.com>
2026-07-08 16:17:27 +0530

    Create Shortest path in binary matrix.java

---
+++ Shortest path in binary matrix.java
@@ -0,0 +1,45 @@
+/*Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+
+A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+
+All the visited cells of the path are 0.
+All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
+The length of a clear path is the number of visited cells of this path.
+time & space: O(n^2) */
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        if (grid[0][0] != 0 || grid[n - 1][n - 1] != 0)
+            return -1; 
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {0,0,1});
+
+        int[][] directions = {
+            {-1,-1}, {-1,0}, {-1,1},
+            {0,-1},         {0,1},
+            {1,-1}, {1,0}, {1,1}
+        };
+        grid[0][0] = 1;
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+            int dist = curr[2];
+
+            if (r == n - 1 && c == n - 1) {
+                return dist;
+            }
+            for (int[] dir: directions) {
+                int nr = r + dir[0];
+                int nc = c + dir[1];
+
+                if (nr >= 0 && nr < n && nc >= 0 && nc < n && grid[nr][nc] == 0) {
+                    grid[nr][nc] = 1;
+                    q.offer(new int[] {nr, nc, dist + 1});
+                }
+            }
+        }
+        return -1;
+    }
+}
